47a9844f26c6ce8e0d4ae353fd766943c560c07c
Viviibarrap <A01642289@itesm.mx>
2021-08-19 00:45:32 +0000

    Tarea 1

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    num1= int(input("Dame el peso inicial: "))
+    num2= int(input("Dame el peso final: "))
+    num3= int(input("Dame la cantidad de meses: "))
+    kilos= (num1-num2)/num3
+    print("Lo que debes bajar por mes es:", kilos)
+    
+
     pass
 
 
